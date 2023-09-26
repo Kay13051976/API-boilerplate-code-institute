178b336824df391929cebde24cd6d527ddea4d3d
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2023-09-26 18:26:27 +0000

    Add display status function to display th API key expiry date status

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,13 +12,27 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data);
+        // Create our display status function and this will take in the parameter of data
+        displayStatus(data)
 
         //to just see expiry date put this instead console.log(data.expiry);
     } else {
 
-        // add an else clause to our if statement and throw an error if the response in not "okay"(test it by put 123 in the end of your API key then check key in JSHinterface)
+        // add an else clause to our if statement and throw an error if the response is not "okay"(test it by put 123 in the end of your API key then check key in JSHinterface)
         throw new Error(data.error);
         
     }
+}
+
+// Display status function
+function displayStatus(data) {
+    let heading = "API Key Status";
+    let results = `<div class="key-status">${data.expiry}</div>`
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
+    //When you click on Check Key on the JSHinterface the API key status expiry date will display
+
 }
