fce8068e5af2c6012bb5dc013d3ad98a75dc54c0
Wanwisa Waddington <kaywanwisa4@googlemail.com>
2023-09-26 19:15:58 +0000

    Create displayErrors function to response to when the response ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const API_KEY ="Q26nWaxQtERy3w8Srveh0WgpfFs";
+const API_KEY = "Q26nWaxQtERy3w8Srveh0WgpfFs";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
@@ -8,8 +8,36 @@ document.getElementById("submit").addEventListener("click", e => postForm(e));
 async function postForm(e) {
     const form = new FormData(document.getElementById("checksform"));
 
-    for (let e of form.entries()) {
-        console.log(e);
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayErrors(data);
+    } else {
+        throw new Error(data.error);
+    }
+
+}
+
+function displayErrors(data) {
+    let heading = `JSHint Results for ${data.file}`;
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span>`
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span></div>, `;
+            results += `column <span class="column">${error.col}</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+
+        }
     }
 }
 
@@ -29,7 +57,7 @@ async function getStatus(e) {
 
         // add an else clause to our if statement and throw an error if the response is not "okay"(test it by put 123 in the end of your API key then check key in JSHinterface)
         throw new Error(data.error);
-        
+
     }
 }
 
